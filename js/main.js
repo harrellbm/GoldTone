b4c8f7a10287d4fa0ba61b38cf759f2e10103231
Brenden Harrell <brendenharrell1@gmail.com>
2020-08-25 13:26:35 -0600

    feat: start of modal for managing communications

--- js/main.js
+++ js/main.js
@@ -108,22 +108,236 @@ function remClicked(e) {
 
 }
 
-const addUserBtnUI = document.getElementById("add-user-btn"); 
-addUserBtnUI.addEventListener("click", addUserBtnClicked);
+/* TODO: add event listner and function to handle adding goal */
 
-function addUserBtnClicked() {
-    const usersRef = dbRef.child('users');
-    const addUserInputsUI = document.getElementsByClassName("user-input");
+/* Add a new communication through modal popup */
+// Get the modal and button that opens it from message manager tab
+      // Note: on the initaitive tab the modal is opened via the beforeCreateSchedule event
+const addCommBtnUI = document.getElementById("add-comm-btn"); 
+const commModalUI = document.getElementById("comm-modal");  
+addCommBtnUI.addEventListener("click", addCommModalLaunch);
+
+// Launch the modal with basic settings. Can take in a date from calendar event to display on creation
+function addCommModalLaunch (calEvent='', launchType='') {
+// will need to handle displaying and editing commmunications later 
+  // Display modal
+  commModalUI.style.display = "block";
+};
+      
+// Get the save button from modal 
+document.getElementById('comm-save-modal').addEventListener("click", commModalSave );
+      
+// Save contents from the modal. Then update Initiative object, Message Manager tab and Initiative tab
+function commModalSave (){
+  const commId = document.getElementById("comm-id");
+  const goalId = document.getElementById("linked-goal-id");
+  const remId = document.getElementById("linked-reminders-id");
+  const type = document.getElementById("comm-type-modal");
+  const subject = document.getElementById("comm-subject-modal");
+  const toWhom = document.getElementById("comm-to-whom-modal");
+  const fromWhom = document.getElementById("comm-from-whom-modal");
+  const date = document.getElementById("comm-date-modal");
+  const sent = document.getElementById("comm-sent-modal");
+  console.log('type', type.value, '\nsubject', subject.value, '\nto whom', toWhom.value, '\nfrom whom', fromWhom.value, '\ndate', date.value, '\nsent', sent.value);
+  // Make sure date and description are filled out 
+  if (date.value != '' && subject.value != ''){
+    // Turn date into moment object to format for adding or updating avenue in initiative object and ui
+    let momDate = moment(date.value, 'YYYY-MM-DD', true); 
+    // If no id provided assume this is a new communication
+          if (commId.value == '' || commId.value == undefined ){
+            // Add avenue to initiative object, initative tab and message manager tab. 
+            addAve('modalAdd', '', 'avenueIn', sent.checked, type.value, description.value, person.value, momDate.toString()); // use Moment date format
+             // Save everything to main
+             let ipcInit = currentInitiative.pack_for_ipc();
+             ipc.send('save', currentInitiativeId, ipcInit);
+          } else {
+            // Update Initiative object 
+            let initAve = currentInitiative.avenues.get(aveId.value); // Avenue object from the initiative object
+            initAve.avenue_type = type.value;
+            initAve.sent = sent.checked;
+            initAve.description = description.value;
+            initAve.person = person.value;
+            initAve.change_date(momDate.toString());
+            // Update Message manager tab
+            let guiAve = document.getElementById(`avenue${aveId.value}`); // Avenue object from the ui
+          
+            guiAve.children[0].value = type.value; // Type
+            guiAve.children[4].children[0].checked = sent.checked; // Sent
+            guiAve.children[5].value = description.value; // Description
+            guiAve.children[6].value = person.value; // Person
+            guiAve.children[7].value = momDate.format('YYYY-MM-DD');
+            // Update Schedule object on calendar 
+            calendar.updateSchedule(aveId.value, '1', {
+              title: description.value,
+              start: momDate.format('ddd DD MMM YYYY HH:mm:ss'),
+              end:  momDate.format('ddd DD MMM YYYY HH:mm:ss')
+            });
+            // Save everything to main
+            let ipcInit = currentInitiative.pack_for_ipc();
+            ipc.send('save', currentInitiativeId, ipcInit);
+          };
+          // Close modal
+          commModalUI.style.display = "none";
+          // Reset modal
+          sent.checked = false;
+          let i, L= type.options.length - 1;
+          for(i = L; i >= 0; i--) {
+            type.remove(i);
+          };
+          date.value = ''; 
+          description.value = '';
+          person.value = '';
+          aveId.value = '';
+          // Reset backgroup of date and description incase they had been changed on unfilled attempt to save
+          date.style.backgroundColor = 'rgb(245, 245,230)';
+          description.style.backgroundColor = 'rgb(245, 245,230)';
+          // Reset modal if it was opened from an avenue connected with a goal 
+          type.disabled = false;
+          description.readOnly = false;
+          date.readOnly = false;
+          document.getElementById('aveDeleteModal').style.display = "block";
+          document.getElementById('aveDeleteModal').style.position = "initial";
+  } else { // Change backgroup of date or description if not filled out 
+      if (date.value == ''){
+        date.style.backgroundColor = 'rgb(225, 160, 140)';
+      };
+      if (subject.value == ''){
+        subject.style.backgroundColor = 'rgb(225, 160, 140)';
+      };
+  };
+};
+    
+// Get the delete button from modal 
+document.getElementById('comm-delete-modal').addEventListener("click", commModalDelete );
+  
+// Delete contents from the modal. Then update Initiative object, Message Manager tab and Initiative tab
+function commModalDelete (){
+  swal({
+    title: 'Deleting Avenue',
+    text: 'Are you sure you want to delete your Avenue?', 
+    icon: 'warning',
+    buttons: ['Cancel', 'Yes'],
+    dangerMode: true
+  })
+  .then(function (value) {
+    if (value == null) { // Escape deletion 
+      return
+    } else { // Proceed with deletion 
+      // Get id from DOM
+      let aveId = document.getElementById('aveIdModal');
+      // Remove avenue from message manager UI
+      let messAve = document.getElementById(`avenue${aveId.value}`);
+      messAve.parentElement.removeChild(messAve);
+      // Delete Schedule object on calendar 
+      calendar.deleteSchedule(aveId.value, '1');
+      // Remove avenue from Initiative object 
+      let id = aveId.value;
+      currentInitiative.avenues.delete(id); // Take only the number off of the end of the ui id
+      // Send updates to main
+      let ipcInit = currentInitiative.pack_for_ipc();
+      ipc.send('save', currentInitiativeId, ipcInit);
+        
+      // Close modal
+      commModalUI.style.display = "none";
+      // Reset modal
+      aveId.value = '';
+      document.getElementById('aveSentModal').checked = false; // Sent
+      let types = document.getElementById('aveDropModal'); // Type
+      let i, L= types.options.length - 1;
+      for(i = L; i >= 0; i--) {
+        types.remove(i);
+      };
+      document.getElementById('avePersModal').value = ''; // Person
+      document.getElementById('aveDateModal').value = ''; // Date Value
+      document.getElementById('aveDescModal').value = ''; // Description Value
+      // Reset backgroup of date and description incase they had been changed on unfilled attempt to save
+      document.getElementById('aveDateModal').style.backgroundColor = 'rgb(245, 245,230)'; // Date Style
+      document.getElementById('aveDescModal').style.backgroundColor = 'rgb(245, 245,230)'; // Description Style
+      return
+    }; 
+  });
+};
+
+// Get the <span> element that closes the modal and attach listener
+document.getElementsByClassName("close")[0].addEventListener("click", function() {
+  commModalUI.style.display = "none";
+  // Refresh calendar 
+  //calendar.render();
+  // Reset modal
+  let sent = document.getElementById('aveSentModal');
+  let type = document.getElementById('aveDropModal');
+  let date = document.getElementById('aveDateModal');
+  let description = document.getElementById('aveDescModal');
+  let person = document.getElementById('avePersModal');
+  let aveId = document.getElementById('aveIdModal');
+  sent.checked = false;
+  let i, L= type.options.length - 1;
+  for(i = L; i >= 0; i--) {
+    type.remove(i);
+  };
+  date.value = ''; 
+  description.value = '';
+  person.value = '';
+  aveId.value = '';
+  // Reset backgroup of date and description incase they had been changed on unfilled attempt to save
+  date.style.backgroundColor = 'rgb(245, 245,230)';
+  description.style.backgroundColor = 'rgb(245, 245,230)';
+  // Reset modal if it was opened from an avenue connected with a goal 
+  type.disabled = false;
+  description.readOnly = false;
+  date.readOnly = false;
+  document.getElementById('aveDeleteModal').style.display = "block";
+  document.getElementById('aveDeleteModal').style.position = "initial";
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.addEventListener('click', function(event) {
+  if (event.target == commModalUI) {
+    commModalUI.style.display = "none";
+    // Refresh calendar 
+    //calendar.render();
+    // Reset modal
+    let sent = document.getElementById('aveSentModal');
+    let type = document.getElementById('aveDropModal');
+    let date = document.getElementById('aveDateModal');
+    let description = document.getElementById('aveDescModal');
+    let person = document.getElementById('avePersModal');
+    let aveId = document.getElementById('aveIdModal');
+    sent.checked = false;
+    let i, L= type.options.length - 1;
+    for(i = L; i >= 0; i--) {
+      type.remove(i);
+    };
+    date.value = ''; 
+    description.value = '';
+    person.value = '';
+    aveId.value = '';
+    // Reset backgroup of date and description incase they had been changed on unfilled attempt to save
+    date.style.backgroundColor = 'rgb(245, 245,230)';
+    description.style.backgroundColor = 'rgb(245, 245,230)';
+    // Reset modal if it was opened from an avenue connected with a goal 
+    type.disabled = false;
+    description.readOnly = false;
+    date.readOnly = false;
+    document.getElementById('aveDeleteModal').style.display = "block";
+    document.getElementById('aveDeleteModal').style.position = "initial";
+  };
+});  
+
+
+function addCommBtnClicked() {
+    const comRef = dbRef.child('communications');
+    const addComInputsUI = document.getElementsByClassName("user-input");
     // this object will hold the new user information 
     let newUser = {};
     // loop through View to get the data for the model 
-    for (let i = 0, len = addUserInputsUI.length; i < len; i++) {
-        let key = addUserInputsUI[i].getAttribute('data-key');
-        let value = addUserInputsUI[i].value;
+    for (let i = 0, len = addComInputsUI.length; i < len; i++) {
+        let key = addComInputsUI[i].getAttribute('data-key');
+        let value = addComInputsUI[i].value;
         newUser[key] = value;
     };
 
-    usersRef.push(newUser, function () {
+    comRef.push(newUser, function () {
         console.log("data has been inserted");
     })
 };
