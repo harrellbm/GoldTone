5616b8e5160d5c70c13d69dc13a31ecd866446c1
Brenden Harrell <brendenharrell1@gmail.com>
2020-09-05 07:18:01 -0600

    style: clean up headings and unused boilerplate code

--- src/index.js
+++ src/index.js
@@ -25,7 +25,10 @@ window.onload = () => {
     }
 } */
 
-/* Initialize firebase */
+let communication_types = ['Email', 'Text', 'Phone Call', 'Facebook', 'Instagram', 'Card', 'Handout', 'Poster','Other'];
+
+
+/*------------------------*/ /* Initialize Firebase */ /*----------------------*/
 const firebaseConfig = {
   apiKey: "AIzaSyAaFDkvt6AA7fvKOLI32ptXNgfkkOyDGN4",
   authDomain: "goldtone-793f5.firebaseapp.com",
@@ -40,9 +43,7 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 
-let communication_types = ['Email', 'Text', 'Phone Call', 'Facebook', 'Instagram', 'Card', 'Handout', 'Poster','Other'];
-
-
+/*---------------------*/ /* Initialize Firebase Auth */ /*--------------------*/
 var uiConfig = {
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
@@ -130,124 +131,8 @@ window.addEventListener('load', function() {
   
 });
 
-window.addEventListener('beforeunload', function(){
-  console.log(firebase.auth().currentUser)
-})
 
-
-
-/**
- * Handles the sign in button press.
- */
-function toggleSignIn() {
-  if (firebase.auth().currentUser) {
-    // [START signout]
-    firebase.auth().signOut();
-    // [END signout]
-  } else {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    if (email.length < 4) {
-      alert('Please enter an email address.');
-      return;
-    }
-    if (password.length < 4) {
-      alert('Please enter a password.');
-      return;
-    }
-    // Sign in with email and pass.
-    // [START authwithemail]
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // [START_EXCLUDE]
-      if (errorCode === 'auth/wrong-password') {
-        alert('Wrong password.');
-      } else {
-        alert(errorMessage);
-      }
-      console.log(error);
-      document.getElementById('quickstart-sign-in').disabled = false;
-      // [END_EXCLUDE]
-    });
-    // [END authwithemail]
-  }
-  document.getElementById('quickstart-sign-in').disabled = true;
-}
-
-/**
- * Handles the sign up button press.
- */
-function handleSignUp() {
-  var email = document.getElementById('email').value;
-  var password = document.getElementById('password').value;
-  if (email.length < 4) {
-    alert('Please enter an email address.');
-    return;
-  }
-  if (password.length < 4) {
-    alert('Please enter a password.');
-    return;
-  }
-  // Create user with email and pass.
-  // [START createwithemail]
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // [START_EXCLUDE]
-    if (errorCode == 'auth/weak-password') {
-      alert('The password is too weak.');
-    } else {
-      alert(errorMessage);
-    }
-    console.log(error);
-    // [END_EXCLUDE]
-  });
-  // [END createwithemail]
-}
-
-/**
- * Sends an email verification to the user.
- */
-function sendEmailVerification() {
-  // [START sendemailverification]
-  firebase.auth().currentUser.sendEmailVerification().then(function() {
-    // Email Verification sent!
-    // [START_EXCLUDE]
-    alert('Email Verification Sent!');
-    // [END_EXCLUDE]
-  });
-  // [END sendemailverification]
-}
-
-function sendPasswordReset() {
-  var email = document.getElementById('email').value;
-  // [START sendpasswordemail]
-  firebase.auth().sendPasswordResetEmail(email).then(function() {
-    // Password Reset Email Sent!
-    // [START_EXCLUDE]
-    alert('Password Reset Email Sent!');
-    // [END_EXCLUDE]
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // [START_EXCLUDE]
-    if (errorCode == 'auth/invalid-email') {
-      alert(errorMessage);
-    } else if (errorCode == 'auth/user-not-found') {
-      alert(errorMessage);
-    }
-    console.log(error);
-    // [END_EXCLUDE]
-  });
-  // [END sendpasswordemail];
-}
-
-
-
+/*-------------------*/ /* Initialize Firebase Database */ /*-----------------*/
 // Grab references to database documents 
 const dbRef = firebase.database().ref();
 
